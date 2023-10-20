de45276e5e162310ecd97ec153915aa789d7341b
Araya <aray2891@mail.ru>
2023-10-20 20:05:50 +0600

    save filtering parameters in url

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,9 @@ import axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
-import { setCategoryId, setCurrentPage } from '../redux/slices/filterSlice';
+import { setCategoryId, setCurrentPage, setFilters } from '../redux/slices/filterSlice';
 import Categories from './../components/Categories';
-import Sort from './../components/Sort';
+import Sort, { sortList } from './../components/Sort';
 import PizzaBlock from './../components/PizzaBlock';
 import Skeleton from './../components/PizzaBlock/Skeleton';
 import Pagination from '../components/Pagination';
@@ -15,6 +15,9 @@ import { SearchContext } from '../App';
 function Home() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const isSearch = React.useRef(false);
+  const isMounted = React.useRef(false);
+
   const { categoryId, sort, currentPage } = useSelector((state) => state.filter);
 
   const { searchValue } = React.useContext(SearchContext);
@@ -30,10 +33,44 @@ function Home() {
 
   const onChangePage = (number) => dispatch(setCurrentPage(number));
 
-  React.useEffect(() => {
+  const fetchPizzas = () => {
     const search = searchValue ? `&search=${searchValue}` : '';
 
     setIsLoading(true);
+
+    axios
+      .get(
+        `https://652d06acf9afa8ef4b26a53e.mockapi.io/items?page=${currentPage}&limit=4&${
+          categoryId > 0 ? `category=${categoryId}` : ''
+        }&sortBy=${sort.sortProperty.replace('-', '')}&order=${
+          sort.sortProperty.includes('-') ? 'asc' : 'desc'
+        }${search}`,
+      )
+      .then((res) => {
+        setItems(res.data);
+        setIsLoading(false);
+      });
+  };
+
+  //Если был первый рендер, то проверяем URL-параметры и сохраняем в redux
+  React.useEffect(() => {
+    if (window.location.search) {
+      const params = qs.parse(window.location.search.substring(1));
+
+      const sort = sortList.find((obj) => obj.sortProperty === params.sortProperty);
+
+      dispatch(
+        setFilters({
+          ...params,
+          sort,
+        }),
+      );
+      isSearch.current = true;
+    }
+  }, []);
+
+  //Если был первый рендер, то запрашиваем пиццы
+  React.useEffect(() => {
     // fetch(
     //   `https://652d06acf9afa8ef4b26a53e.mockapi.io/items?page=${currentPage}&limit=4&${
     //     categoryId > 0 ? `category=${categoryId}` : ''
@@ -48,31 +85,27 @@ function Home() {
     //     setItems(json);
     //     setIsLoading(false);
     //   });
-
-    axios
-      .get(
-        `https://652d06acf9afa8ef4b26a53e.mockapi.io/items?page=${currentPage}&limit=4&${
-          categoryId > 0 ? `category=${categoryId}` : ''
-        }&sortBy=${sort.sortProperty.replace('-', '')}&order=${
-          sort.sortProperty.includes('-') ? 'asc' : 'desc'
-        }${search}`,
-      )
-      .then((res) => {
-        setItems(res.data);
-        setIsLoading(false);
-      });
     window.scrollTo(0, 0);
+
+    if (!isSearch.current) {
+      fetchPizzas();
+    }
+
+    isSearch.current = false;
   }, [categoryId, sort.sortProperty, searchValue, currentPage]);
 
+  //Если изменились параметры и был первый рендер
   React.useEffect(() => {
-    const queryString = qs.stringify({
-      sortProperty: sort.sortProperty,
-      categoryId,
-      currentPage,
-    });
-
-    navigate(`?${queryString}`);
-  }, [categoryId, sort.sortProperty, searchValue, currentPage]);
+    if (isMounted.current) {
+      const queryString = qs.stringify({
+        sortProperty: sort.sortProperty,
+        categoryId,
+        currentPage,
+      });
+      navigate(`?${queryString}`);
+    }
+    isMounted.current = true;
+  }, [categoryId, sort.sortProperty, currentPage]);
 
   const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
   const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
