1e8042b87762e03f8d91a23ef8ea31767cdf02f1
Araya <aray2891@mail.ru>
2023-10-24 14:33:44 +0600

    creating a separate page for each pizza using useParams

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
+import FullPizza from './pages/FullPizza';
 
 import './scss/app.scss';
 import NotFound from './pages/NotFound';
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/cart" element={<Cart />}></Route>
+          <Route path="/pizza/:id" element={<FullPizza />}></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import qs from 'qs';
 
 import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 import { setCategoryId, setCurrentPage, setFilters } from '../redux/slices/filterSlice';
 import { fetchPizzas } from '../redux/slices/pizzasSlice';
@@ -113,7 +113,11 @@ function Home() {
     isMounted.current = true;
   }, [categoryId, sort.sortProperty, currentPage]);
 
-  const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+  const pizzas = items.map((obj) => (
+    <Link key={obj.id} to={`/pizza/${obj.id}`}>
+      <PizzaBlock {...obj} />
+    </Link>
+  ));
   const skeletons = [...new Array(4)].map((_, index) => <Skeleton key={index} />);
 
   return (
